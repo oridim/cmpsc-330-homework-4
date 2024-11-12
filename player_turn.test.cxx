c6fffcefcf5bef8dd6b26f6beabbc38f0f899d2d
oridim <125223080+oridim@users.noreply.github.com>
2024-11-12 20:36:38 +0000

    fix: unit test using the wrong variables

--- player_turn.test.cxx
+++ player_turn.test.cxx
@@ -15,7 +15,7 @@ int main()
 
      cout << "playerTurn1 = PlayerTurn(3, 2, 4, 'D'): " << endl
           << endl
-          << "\tplayerTurn1.turnIndex()\t= " << playerTurn1.turnIndex() << "\t(SHOULD BE: 3)" << endl
+          << "\tplayerTurn1.turnIndex()\t\t= " << playerTurn1.turnIndex() << "\t(SHOULD BE: 3)" << endl
           << "\tplayerTurn1.row()\t\t= " << playerTurn1.rowIndex() << "\t(SHOULD BE: 2)" << endl
           << "\tplayerTurn1.column()\t\t= " << playerTurn1.columnIndex() << "\t(SHOULD BE: 4)" << endl
           << "\tplayerTurn1.playerInitial()\t= '" << playerTurn1.playerInitial() << "'" << "\t(SHOULD BE: 'D')" << endl;
@@ -33,11 +33,10 @@ int main()
 
      cout << "playerTurnCopy = PlayerTurn(playerTurn1): " << endl
           << endl
-          << "\tplayerTurn1.turnIndex()\t= " << playerTurn1.turnIndex() << "\t(SHOULD BE: 3)" << endl
-          << "\tplayerTurn1.row()\t\t= " << playerTurn1.rowIndex() << "\t(SHOULD BE: 2)" << endl
-          << "\tplayerTurn1.column()\t\t= " << playerTurn1.columnIndex() << "\t(SHOULD BE: 4)" << endl
-          << "\tplayerTurn1.playerInitial()\t= '" << playerTurn1.playerInitial() << "'" << "\t(SHOULD BE: 'D')" << endl
-          << endl;
+          << "\tplayerTurnCopy.turnIndex()\t= " << playerTurnCopy.turnIndex() << "\t(SHOULD BE: 3)" << endl
+          << "\tplayerTurnCopy.row()\t\t= " << playerTurnCopy.rowIndex() << "\t(SHOULD BE: 2)" << endl
+          << "\tplayerTurnCopy.column()\t\t= " << playerTurnCopy.columnIndex() << "\t(SHOULD BE: 4)" << endl
+          << "\tplayerTurnCopy.playerInitial()\t= '" << playerTurnCopy.playerInitial() << "'" << "\t(SHOULD BE: 'D')" << endl;
 
      return 0;
 }
