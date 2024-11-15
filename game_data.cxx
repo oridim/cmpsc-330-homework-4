547dd762cf891531aa5586c354f872d227c109df
ozwsc <181761291+ozwsc@users.noreply.github.com>
2024-11-15 11:19:06 -0500

    Comments: game_data

--- game_data.cxx
+++ game_data.cxx
@@ -9,8 +9,11 @@
 
 #include "game_data.h"
 
+// Function to read game data from input stream and create new GameData object.
 GameData *GameData::readGameData(istream &inputStream)
 {
+    // Variables to store the number of columns and rows in the game board.
+    // Creation of a new SimpleVector to store the sequence of player turns. 
     int columns;
     int rows;
     SimpleVector<PlayerTurn> *turns = new SimpleVector<PlayerTurn>();
@@ -20,9 +23,11 @@ GameData *GameData::readGameData(istream &inputStream)
     int columnIndex;
     int rowIndex;
 
+    // Variable to store the player's initial (character) and turn counter.
     string playerInitial;
     int turnIndex = 0;
 
+    // Loop to read player turns from the input stram until "END" is encountered. 
     while (inputStream >> playerInitial)
     {
         if (playerInitial == "END")
@@ -30,10 +35,14 @@ GameData *GameData::readGameData(istream &inputStream)
             break;
         }
 
+        // Read the row and column indices for the current turn.
         inputStream >> rowIndex >> columnIndex;
 
+        // Create new PlayerTurn object for the current turn
         PlayerTurn *playerTurn = new PlayerTurn(turnIndex, rowIndex, columnIndex, toupper(playerInitial.at(0)));
 
+        // Add PlayerTurn object to the vector of turns
+        // Increment the turn index
         turns->push_back(*playerTurn);
         turnIndex += 1;
     }
@@ -41,6 +50,7 @@ GameData *GameData::readGameData(istream &inputStream)
     return new GameData(columns, rows, turns);
 }
 
+// Constructor for the GameData class
 GameData::GameData(int rows, int columns, SimpleVector<PlayerTurn> *turns)
 {
     this->_columns = columns;
@@ -48,30 +58,34 @@ GameData::GameData(int rows, int columns, SimpleVector<PlayerTurn> *turns)
     this->_turns = turns;
 }
 
+// Destructor for the GameData class
 GameData::~GameData()
 {
     delete this->_turns;
 }
 
+// Equality operator for GameData objects
 bool GameData::operator==(const GameData &rightHandData) const
 {
-    // Usually the convention in most OOP languages I have done equality checking
-    // is done by default as nominal instances rather then structural values.
-    //
-    // So we will just compare pointers here for nominal instance checking.
+
+    // Compare pointers for nominal instance checking
+    // This checks if the two GameData objects are the same instance.
     return this == &rightHandData;
 }
 
+// Inequality operator for GameData
 bool GameData::operator!=(const GameData &rightHandData) const
 {
     // See `GameData::operator==` for reasoning.
     return this != &rightHandData;
 }
 
+// Function to create a GameBoard object from the stored game data
 GameBoard *GameData::makeGameBoard()
 {
     GameBoard *gameBoard = new GameBoard(this->_rows, this->_columns);
 
+    // Apply each player turn from the stored turns verctor to the Gameboard.
     for (int index = 0; index < this->_turns->size(); index++)
     {
         const PlayerTurn &turn = this->_turns->at(index);
