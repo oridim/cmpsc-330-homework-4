69918ef80aa5fb07b97b9f0157cc08ce70e131ea
ozwsc <181761291+ozwsc@users.noreply.github.com>
2024-11-15 12:53:47 -0500

    Comments: game_board.cxx

--- game_board.cxx
+++ game_board.cxx
@@ -17,22 +17,25 @@ using namespace std;
 
 GameBoard::GameBoard(int rows, int columns)
 {
-    // **IMPORTANT**: We need to assign these values before we call computed
-    // data getters! Otherwise they will calculate garbage values!
+    // Initialize the rows and columns of the game board. 
+    // These values must be set before computing derived properties
     this->_columns = columns;
     this->_rows = rows;
 
     int expandedRows = this->expandedRows();
     int expandedColumns = this->expandedColumns();
 
+    // Create a grid (2D vector) of GameBoard slot objects with the dimensions.
     SimpleVector<SimpleVector<GameBoardSlot>> *gridRows =
         new SimpleVector<SimpleVector<GameBoardSlot>>(expandedRows);
 
+    // Initialize wach row of the grid.
     for (int rowIndex = 0; rowIndex < gridRows->size(); rowIndex++)
     {
         SimpleVector<GameBoardSlot> *gridColumns =
             new SimpleVector<GameBoardSlot>(expandedColumns);
 
+        // Initialize each column of the grid and determine the type of slot.
         for (int columnIndex = 0; columnIndex < gridColumns->size(); columnIndex++)
         {
             GameBoardSlot::SLOT_KIND slotKind = GameBoardSlot::determineSlotKind(rowIndex, columnIndex);
@@ -50,15 +53,14 @@ GameBoard::GameBoard(int rows, int columns)
 
 GameBoard::~GameBoard()
 {
+    // Deallocate the memory for the grid to avoid memory leaks
     delete this->_grid;
 }
 
 bool GameBoard::operator==(const GameBoard &rightHandBoard) const
 {
-    // Usually the convention in most OOP languages I have done equality checking
-    // is done by default as nominal instances rather then structural values.
-    //
-    // So we will just compare pointers here for nominal instance checking.
+    // Check for equality of two GameBoard instances. The comparison is nominal,
+    // meaning it only checks if the two objects are the same instance in the memory.
     return this == &rightHandBoard;
 }
 
@@ -70,29 +72,35 @@ bool GameBoard::operator!=(const GameBoard &rightHandBoard) const
 
 int GameBoard::columnPadding() const
 {
+    // Compute the padding between columns for expanded dimensions.
     return max(this->_columns - 1, 0);
 }
 
 int GameBoard::rowPadding() const
 {
+    // Compute the padding between rows for expanded dimensions.
     return max(this->_rows - 1, 0);
 }
 
 int GameBoard::expandedColumns() const
 {
+    // Calculate the total number of columns including padding.
     return this->_columns + this->columnPadding();
 }
 
 int GameBoard::expandedRows() const
 {
+    // Calculate the total number of rows including padding.
     return this->_rows + this->rowPadding();
 }
 
 const PlayerTurn *GameBoard::_determineCapturePriorityTurn(int rowIndex, int columnIndex)
 {
+    // Determine the PlayerTurn with the highest priority for a potential capture.
     const PlayerTurn *priorityTurn = nullptr;
     SimpleVector<SimpleVector<GameBoardSlot>> *gridRows = this->_grid;
 
+    // Check the rows above and below the current position for valid line slots. 
     for (int deltaRowIndex = -1; deltaRowIndex <= 1; deltaRowIndex += 2)
     {
         SimpleVector<GameBoardSlot> &gridColumns = gridRows->at(rowIndex + deltaRowIndex);
@@ -111,6 +119,7 @@ const PlayerTurn *GameBoard::_determineCapturePriorityTurn(int rowIndex, int col
         }
     }
 
+    // Check the columns left and right of the current position for the valid line slots. 
     for (int deltaColumnIndex = -1; deltaColumnIndex <= 1; deltaColumnIndex += 2)
     {
         SimpleVector<GameBoardSlot> &gridColumns = gridRows->at(rowIndex);
@@ -134,9 +143,14 @@ const PlayerTurn *GameBoard::_determineCapturePriorityTurn(int rowIndex, int col
 
 void GameBoard::applyScorableCaptures()
 {
+    // This method iterates through all scorable slots on the game board and applies captures
+    // if a specific player has priority based on game rules. 
+
+    // Retrieve the grid and compute all scorable slots.
     SimpleVector<SimpleVector<GameBoardSlot>> *gridRows = this->_grid;
     SimpleVector<GameBoardSlot> *scorableSlots = this->computeScorableSlots();
 
+    // Loop through each scorable slot.
     for (int index = 0; index < scorableSlots->size(); index++)
     {
         GameBoardSlot &scorableBoardSlot = scorableSlots->at(index);
@@ -144,14 +158,17 @@ void GameBoard::applyScorableCaptures()
         int columnIndex = scorableBoardSlot.columnIndex();
         int rowIndex = scorableBoardSlot.rowIndex();
 
+        // Determine the priority player turn for capturing this slot
         const PlayerTurn *priorityTurn = this->_determineCapturePriorityTurn(
             rowIndex, columnIndex);
 
+        // If no priority player turn exists, then skip to the next slot.
         if (priorityTurn == nullptr)
         {
             continue;
         }
 
+        // Access the slot in the grid and apply the update to mark it captured.
         SimpleVector<GameBoardSlot> &gridColumns = gridRows->at(rowIndex);
         GameBoardSlot &oldGameBoardSlot = gridColumns.at(columnIndex);
 
@@ -164,12 +181,17 @@ void GameBoard::applyScorableCaptures()
 
 void GameBoard::applyTurn(const PlayerTurn &playerTurn)
 {
+    // This method applies a player's move to the gameboard.
+
+    // Retrieve the indices of the player's moves.
     int columnIndex = playerTurn.columnIndex();
     int rowIndex = playerTurn.rowIndex();
 
+    // Access the grid and retrieve the corresponding slot for the move.
     SimpleVector<SimpleVector<GameBoardSlot>> *gridRows = this->_grid;
     SimpleVector<GameBoardSlot> &gridColumns = gridRows->at(rowIndex);
 
+    // Check if slot it a legal move, if not throw an exception
     GameBoardSlot &oldBoardSlot = gridColumns.at(columnIndex);
 
     if (!oldBoardSlot.isLegalMove())
@@ -177,15 +199,20 @@ void GameBoard::applyTurn(const PlayerTurn &playerTurn)
         throw string("bad argument #0 to 'GameBoard.applyTurn(const PlayerTurn &)' (gameboard grid slot at [") + to_string(rowIndex) + string(", ") + to_string(columnIndex) + string("] is not a legal move)");
     }
 
+    // Apply the move, marking the slot as a line belonging to the player.
     oldBoardSlot._applyUpdate(GameBoardSlot::SLOT_KIND::line, playerTurn);
 }
 
 SimpleVector<GameBoardSlot> *GameBoard::computeKindSlots(
     GameBoardSlot::SLOT_KIND slotKind) const
 {
+    // This method retrieves all slots of a specific kind from the game board.
+
+    // Allocate memory for storing the matching slots. 
     SimpleVector<SimpleVector<GameBoardSlot>> *gridRows = this->_grid;
     SimpleVector<GameBoardSlot> *kindSlots = new SimpleVector<GameBoardSlot>();
 
+    // Iterate through the entire gird to find slots mathcing the specified kind.
     for (int rowIndex = 0; rowIndex < gridRows->size(); rowIndex++)
     {
         SimpleVector<GameBoardSlot> &gridColumns = gridRows->at(rowIndex);
@@ -195,6 +222,7 @@ SimpleVector<GameBoardSlot> *GameBoard::computeKindSlots(
             GameBoardSlot &gameBoardSlot = gridColumns.at(columnIndex);
             if (gameBoardSlot.slotKind() == slotKind)
             {
+                // Add the matching slot to the list.
                 kindSlots->push_back(gameBoardSlot);
             }
         }
@@ -217,6 +245,7 @@ SimpleVector<GameBoardSlot> *GameBoard::computeLegalSlots() const
     SimpleVector<SimpleVector<GameBoardSlot>> *gridRows = this->_grid;
     SimpleVector<GameBoardSlot> *legalSlots = new SimpleVector<GameBoardSlot>();
 
+    // Check slots in rows with even indices, starting at column 1, incrementing by 2.
     for (int rowIndex = 0; rowIndex < gridRows->size(); rowIndex += 2)
     {
         SimpleVector<GameBoardSlot> &gridColumns = gridRows->at(rowIndex);
@@ -232,6 +261,7 @@ SimpleVector<GameBoardSlot> *GameBoard::computeLegalSlots() const
         }
     }
 
+    // Check slots in rows with odd indices, starting at column 0, incrementing by 2.
     for (int rowIndex = 1; rowIndex < gridRows->size(); rowIndex += 2)
     {
         SimpleVector<GameBoardSlot> &gridColumns = gridRows->at(rowIndex);
@@ -262,9 +292,12 @@ SimpleVector<GameBoardSlot> *GameBoard::computeScorableSlots(
     // we do not have to check every gameboard slot if they are of
     // `GameBoardSlot::SLOT_KIND::initial` or `GameBoardSlot::SLOT_KIND::scorable` kind.
 
+    // Allocate memory to store scorable slots.
     SimpleVector<SimpleVector<GameBoardSlot>> *gridRows = this->_grid;
     SimpleVector<GameBoardSlot> *scorableSlots = new SimpleVector<GameBoardSlot>();
 
+    // Scorable slots are located at odd-indexed rows and columns (1,1), (3,3)...
+    // Incrementing by two reduces unnecessary checks.
     for (int rowIndex = 1; rowIndex < gridRows->size(); rowIndex += 2)
     {
         SimpleVector<GameBoardSlot> &gridColumns = gridRows->at(rowIndex);
@@ -273,6 +306,7 @@ SimpleVector<GameBoardSlot> *GameBoard::computeScorableSlots(
         {
             GameBoardSlot &gameBoardSlot = gridColumns.at(columnIndex);
 
+            // Check if the current slot matches the desired slot kind.
             if (gameBoardSlot.slotKind() == slotKind)
             {
                 scorableSlots->push_back(gameBoardSlot);
@@ -285,6 +319,9 @@ SimpleVector<GameBoardSlot> *GameBoard::computeScorableSlots(
 
 const GameBoardSlot &GameBoard::getSlot(int rowIndex, int columnIndex) const
 {
+    // This method retrieves a slot from the game board grid based on row and column indices.
+
+    // Access the grid rows and retrieve the specific column in the given row.
     SimpleVector<SimpleVector<GameBoardSlot>> *gridRows = this->_grid;
     SimpleVector<GameBoardSlot> &gridColumns = gridRows->at(rowIndex);
 
@@ -293,6 +330,8 @@ const GameBoardSlot &GameBoard::getSlot(int rowIndex, int columnIndex) const
 
 void GameBoard::renderGameBoard() const
 {
+    //This method renders the entire game board, including row and column legends
+    //
     // We create a prefixing line that contains a legend along the top of columns of
     // the gameboard grid.
     //
@@ -317,10 +356,12 @@ void GameBoard::renderGameBoard() const
 
     cout << endl;
 
+    // Print secondary column legend in a sequence of 0 to 9.
     for (int columnIndex = 0; columnIndex <= this->expandedColumns(); columnIndex++)
     {
         if (columnIndex == 0)
         {
+            // Leave a blank space for row alignment.
             cout << "   ";
             continue;
         }
@@ -342,6 +383,7 @@ void GameBoard::renderGameBoard() const
 
     for (int rowIndex = 0; rowIndex < gridRows->size(); rowIndex++)
     {
+        // Print row legends, similar to column legends, but for rows.
         int rowLegend = rowIndex % 10;
         int rowUberLegend = rowIndex / 10;
 
@@ -354,12 +396,14 @@ void GameBoard::renderGameBoard() const
             cout << " " << rowLegend << " ";
         }
 
+        // Render the contents of the current row.
         SimpleVector<GameBoardSlot> &gridColumns = gridRows->at(rowIndex);
 
         for (int columnIndex = 0; columnIndex < gridColumns.size(); columnIndex++)
         {
             GameBoardSlot &gameBoardSlot = gridColumns.at(columnIndex);
-
+            
+            // Print the display character of each slot
             cout << gameBoardSlot.displayCharacter();
         }
 
