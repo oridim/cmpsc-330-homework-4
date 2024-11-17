fda6946255f7bf49319d876c8f193fbe99f651e9
oridim <125223080+oridim@users.noreply.github.com>
2024-11-17 22:43:01 +0000

    feat: added script for running `dotsandboxes` with sample input

---
+++ run-dotsandboxes.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+
+# **NOTE**: The below flag causes the Bash script to exit whenever a command fails.
+
+set -e
+
+# Run w/ Sample Input
+
+./dotsandboxes < "./Test Cases/GameConfiguration/configuration.professor-supplied.txt"
