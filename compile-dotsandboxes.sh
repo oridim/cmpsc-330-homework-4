d9a450c6f29ed6da17cdbb0cd3d9f816db7282a7
oridim <125223080+oridim@users.noreply.github.com>
2024-11-17 22:31:56 +0000

    chore: added script for compiling

---
+++ compile-dotsandboxes.sh
@@ -0,0 +1,21 @@
+#!/usr/bin/env bash
+
+# **NOTE**: The below flag causes the Bash script to exit whenever a command fails.
+
+set -e
+
+# Compile Unit Tests
+
+/usr/bin/g++-11 -ansi -pedantic -std=c++14 -Wall -g \
+    "./basic_strategy_ai_player.cxx" \
+    "./game_configuration.cxx" \
+    "./game_board.cxx" \
+    "./game_board_slot.cxx" \
+    "./game_data.cxx" \
+    "./game_result.cxx" \
+    "./game_session.cxx" \
+    "./player_move.cxx" \
+    "./player_result.cxx" \
+    "./player_turn.cxx" \
+    "./random_ai_player.cxx" \
+    "./main.cxx" -o "./dotsandboxes"
